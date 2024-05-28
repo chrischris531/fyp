71dad63cda77eb02eaf13b5cd4ef255f00c07587
Chris Myers <cm1220@ic.ac.uk>
2024-05-28 16:48:58 +0100

    testing on doc

--- toy_datasets.py
+++ toy_datasets.py
@@ -30,6 +30,10 @@ class ToyDatasets():
   def stationary_with_non_gaussian_noise(self, dof=3, offset=5, noise_factor=10):
     dataset = offset + np.random.standard_t(dof, self.num_elements) / noise_factor
     return dataset
+  
+  def stationary_with_both_noise(self, offset=5, std=0.1, gnoise_factor=1, dof=3, ngnoise_factor=10):
+    dataset = np.random.normal(offset, std, self.num_elements) / gnoise_factor + np.random.standard_t(dof, self.num_elements) / ngnoise_factor
+    return dataset
 
   def linear_trending_without_noise(self, m=2, offset=5):
     dataset = (np.arange(self.num_elements) / self.num_elements - 1/m) * m + offset
@@ -44,6 +48,11 @@ class ToyDatasets():
     linear_trending = self.linear_trending_without_noise(m=m, offset=offset)
     dataset = linear_trending + np.random.standard_t(dof, self.num_elements) / noise_factor
     return dataset
+  
+  def linear_trending_with_both_noise(self, m=2, offset=5, std=0.1, gnoise_factor=1, dof=3, ngnoise_factor=10):
+    dataset = self.linear_trending_with_gaussian_noise(m=m, offset=offset, std=0.1, noise_factor=gnoise_factor)
+    dataset = dataset + np.random.standard_t(dof, self.num_elements) / ngnoise_factor
+    return dataset    
   
   def periodical_linear_without_noise(self, m=2, offset=5):
     dataset = np.tile(
@@ -63,6 +72,11 @@ class ToyDatasets():
     periodical = self.periodical_linear_without_noise(m, offset)
     dataset = periodical + np.random.standard_t(dof, self.num_elements) / noise_factor
     return dataset
+
+  def periodical_linear_with_both_noise(self, m=2, offset=5, std=0.1, gnoise_factor=1, dof=3, ngnoise_factor=10):
+    dataset = self.periodical_linear_with_gaussian_noise(m=m, offset=offset, std=std, noise_factor=gnoise_factor)
+    dataset = dataset + np.random.standard_t(dof, self.num_elements) / ngnoise_factor
+    return dataset
   
   def periodical_sinusoidal_without_noise(self, offset=5, amplitude=1, frequency=0.1, phase=0):
     dataset = offset + amplitude * np.sin(frequency * np.arange(self.num_elements) + phase)
@@ -76,6 +90,11 @@ class ToyDatasets():
   def periodical_sinusoidal_with_non_gaussian_noise(self, offset=5, amplitude=1, frequency=0.1, phase=0, dof=3, noise_factor=10):
     periodical = self.periodical_sinusoidal_without_noise(offset, amplitude, frequency, phase)
     dataset = periodical + np.random.standard_t(dof, self.num_elements) / noise_factor
+    return dataset
+
+  def periodical_sinusoidal_with_both_noise(self, offset=5, amplitude=1, frequency=0.1, phase=0, std=0.1, gnoise_factor=1, dof=3, ngnoise_factor=10):
+    dataset = self.periodical_sinusoidal_with_gaussian_noise(offset=offset, amplitude=amplitude, frequency=frequency, phase=phase, std=std, noise_factor=gnoise_factor)
+    dataset = dataset + np.random.standard_t(dof, self.num_elements) / ngnoise_factor
     return dataset
 
   def polynomial_without_noise(self, order=2, min_x=-1, max_x=1, y_offset=1):
